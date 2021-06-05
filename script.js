32374b15d4681f60185171e2f150d51f5b04e707
Vankir94 <67509982+Vankir94@users.noreply.github.com>
2021-06-05 19:12:04 +0300

    Slider

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.querySelector('.btn-popup');
 const popup = document.querySelector('.popup');
 const clos = document.querySelector('.popup-close');
+
+
 button.addEventListener('click', () => {
     popup.classList.add('popup_open');
 });
@@ -10,5 +12,12 @@ clos.addEventListener('click', () => {
 
 //slider
 
+let slides = document.querySelectorAll('#slides .slide');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide,4000);
 
-
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
